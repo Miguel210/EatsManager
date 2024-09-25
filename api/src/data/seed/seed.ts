71763e1f37715660dd250f5ad468f7eba5d9e1d9
Miguel210 <mickyjunior04@gmail.com>
2024-09-25 15:22:11 -0600

    New data added to seed.ts and data.ts

--- api/src/data/seed/seed.ts
+++ api/src/data/seed/seed.ts
@@ -21,11 +21,51 @@ async function main() {
         data: seedData.module,
     });
 
-    console.log('SEEED');
+    let data1 = await prisma.module.findMany();
     
+    const operationsData = seedData.operation;
+    for(let operation of operationsData){
+        await prisma.operation.create({
+            data: {
+                id: operation.id,
+                name: operation.name,
+                moduleId: data1[randomBetween0andX(modules.count)].id,
+                isActive: true
+            }
+        })
+    }
+
+    const profiles = await prisma.profile.createMany({
+        data: seedData.profile
+    })
+
+    const data2 = await prisma.profile.findMany();
+    const data3 = await prisma.operation.findMany();
+    for(let profileOpr of seedData.profileoperation){
+        await prisma.profileOperation.create({
+            data: {
+                id: profileOpr.id,
+                profileId: data2[randomBetween0andX(profiles.count)].id,
+                operationId: data3[randomBetween0andX(10)].id,
+                createAt: profileOpr.createAt,
+                isActive: profileOpr.isActive
+
+            }
+        })
+    }
 }
 
 const randomBetween0andX = (x: number) => {
     return Math.floor(Math.random() * x)
 }
 
+/*
+    await prisma.operation.createMany({
+        data: {
+            id: seedData.modules[randomBetween0andX(modules.length)].id,
+            moduleId: '1',
+            name: '21',
+            isActive: true
+        }
+    })
+*/
