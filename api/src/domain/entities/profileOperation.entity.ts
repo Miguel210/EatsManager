89cfceb90e98004a4fbe77a4346686c801a9d1bf
Miguel210 <mickyjunior04@gmail.com>
2024-09-30 11:09:33 -0600

    Created ProfileOperationEntity

---
+++ api/src/domain/entities/profileOperation.entity.ts
@@ -0,0 +1,30 @@
+import { CustomError } from "../errors/custom.error";
+
+
+
+
+export class ProfileOperation {
+
+    constructor(
+        public id: String,
+        public profileId: String,
+        public operationId: String
+    ){}
+
+    static fromObject(object: {[key: string]: any}) {
+
+        const {id, profileId, operationId} = object;
+
+        if( !id ) {
+            throw CustomError.badRequest('Missing id ProfileOperation');
+        }
+        if( !profileId ) {
+            throw CustomError.badRequest('Missing id profileId');
+        }
+        if( !operationId ) {
+            throw CustomError.badRequest('Missing id operationId');
+        }
+        
+        return new ProfileOperation(id, profileId, operationId)
+    }
+}
