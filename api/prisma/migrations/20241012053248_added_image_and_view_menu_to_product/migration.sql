96bbf979c6762720d71f491d9f6889e6d4aa1b13
Miguel Angel Lozano Alvarez <mickyjunior04@gmail.com>
2024-10-11 23:37:28 -0600

    Added image and viewMenu fields in the product table

---
+++ api/prisma/migrations/20241012053248_added_image_and_view_menu_to_product/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Added the required column `image` to the `product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "product" ADD COLUMN     "image" TEXT NOT NULL,
+ADD COLUMN     "viewMenu" BOOLEAN NOT NULL DEFAULT false;
