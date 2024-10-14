7004cf0a89ce82d6d9e0e32bc43fcaf1979bdcdc
Miguel210 <mickyjunior04@gmail.com>
2024-10-14 11:54:15 -0600

    Changed product.entity.ts (added viewMenu, isActive, image, deleteAt and isDelete)

--- api/src/domain/entities/product.entity.ts
+++ api/src/domain/entities/product.entity.ts
@@ -11,12 +11,17 @@ export class ProductEntity {
         public existence: number,
         public price: number,
         public productTypeId: string,
-        public category: string
+        public category: string,
+        public viewMenu: boolean,
+        public isActive: boolean,
+        public image: string,
+        public deleteAt: Date,
+        public isDelete: boolean
     ) {}
 
     static fromObject(object: {[key: string]: any}) {
 
-        const {id, code, description, existence, price, productTypeId, category} = object
+        const {id, code, description, existence, price, productTypeId, category, viewMenu, isActive, image, deleteAt, isDelete } = object
 
         if( !id ) throw CustomError.badRequest('Missig id product');
 
@@ -26,8 +31,11 @@ export class ProductEntity {
         if( !price ) throw CustomError.badRequest('Missig price');
         if( !productTypeId ) throw CustomError.badRequest('Missig productTypeId');
         if( !category ) throw CustomError.badRequest('Missig category');
+        if( !viewMenu ) throw CustomError.badRequest('Missing viewMenu')
+        if( !isActive ) throw CustomError.badRequest('Missing isActive')
 
+        if(isDelete === false) throw CustomError.badRequest('No exist')
 
-        return new ProductEntity(id, code, description, existence, price, productTypeId, category)
+        return new ProductEntity(id, code, description, existence, price, productTypeId, category, viewMenu, isActive, image, deleteAt, isDelete)
     }
 }
