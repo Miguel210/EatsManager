2ef3ea486464a886bc3a977b4d495d8041f72731
Miguel210 <mickyjunior04@gmail.com>
2024-10-18 12:35:43 -0600

    Created updateOfferDto and createOfferDto  files

---
+++ api/src/domain/dtos/offer/create-offer.ts
@@ -0,0 +1,24 @@
+export class CreateOfferDto{
+
+    private constructor(
+        public readonly productId: string,
+        public readonly price: number,
+        public readonly from: Date,
+        public readonly until: Date,
+        public readonly isActive: boolean
+    ) {}
+
+    static fromObject(props: {[key: string]: any}): [string?, CreateOfferDto?] {
+
+        const {productId, price, from, until, isActive} = props
+    
+
+        if( !productId ) return ['ProductId is requerid'];
+        if( !price ) return ['Price is requerid'];
+        if( !from ) return ['From (date) is requerid'];
+        if( !until ) return ['Until (date) is requerid'];
+        if( isActive === undefined ) return ['IsActive is requerid'];
+
+        return [undefined, new CreateOfferDto(productId , price, from, until, isActive)]
+    }
+}
---
+++ api/src/domain/dtos/offer/update-offer.ts
@@ -0,0 +1,25 @@
+export class UpdateOfferDto{
+
+    private constructor(
+        public readonly id: string,
+        public readonly productId: string,
+        public readonly price: number,
+        public readonly from: Date,
+        public readonly until: Date,
+        public readonly isActive: boolean
+    ) {}
+
+    static fromObject(props: {[key: string]: any}): [string?, UpdateOfferDto?] {
+
+        const {id, productId, price, from, until, isActive} = props
+    
+        if( !id ) return ['Id is requerid'];
+        if( !productId ) return ['ProductId is requerid'];
+        if( !price ) return ['Price is requerid'];
+        if( !from ) return ['From (date) is requerid'];
+        if( !until ) return ['Until (date) is requerid'];
+        if( isActive === undefined ) return ['IsActive is requerid'];
+
+        return [undefined, new UpdateOfferDto(id, productId , price, from, until, isActive)]
+    }
+}
