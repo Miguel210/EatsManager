2fa6402faafda0f36a15d176289f85fe07d4be77
Miguel210 <mickyjunior04@gmail.com>
2024-10-18 13:01:37 -0600

    Created offerDatasource and offerRepository  files

---
+++ api/src/domain/datasource/offer/offer.datasoruce.ts
@@ -0,0 +1,14 @@
+import { CreateOfferDto } from "../../dtos/offer/create-offer";
+import { UpdateOfferDto } from "../../dtos/offer/update-offer";
+import { OfferEntity } from "../../entities/offer.entity";
+
+
+
+export abstract class OfferDatasource {
+    abstract create(dto: CreateOfferDto): Promise <OfferEntity>;
+
+    abstract getOfferbyId(id: string): Promise <OfferEntity>;
+    abstract getOffers(): Promise <OfferEntity[]>;
+    abstract update(dto: UpdateOfferDto): Promise <OfferEntity>;
+    abstract delete(id: string): Promise <OfferEntity>;
+}
