3e58370ccfa112d0dd2cb0e46f017a9e05a9e117
Miguel210 <mickyjunior04@gmail.com>
2024-10-21 00:25:29 -0600

    Created attendanceEntity file

---
+++ api/src/domain/entities/attendance.entity.ts
@@ -0,0 +1,33 @@
+import { CustomError } from "../errors/custom.error";
+
+
+
+
+export class AttendanceEntity {
+
+    constructor(
+        public id: string,
+        public employeeId: string,
+        public date: Date,
+        public hour: Date,
+        public documentId: string,
+        public isActive: boolean
+    ) {}
+
+
+    static fromObject(object: {[key: string]: any}) {
+
+        const {id, employee, employeeId, date, hour, document, documentId, isActive} = object;
+
+        if( !id ) throw CustomError.badRequest('Missing id attendance');
+
+        if( !employee && employeeId ) throw CustomError.badRequest('Missing employeeId or employee');
+        if( !date ) throw CustomError.badRequest('Missing date');
+        if( !hour ) throw CustomError.badRequest('Missing hour');
+        if( !document && documentId ) throw CustomError.badRequest('Missing documentId or document');
+        if( isActive === undefined ) throw CustomError.badRequest('Missing isActive');
+
+
+        return new AttendanceEntity(id, employee || employeeId, date, hour, document || documentId, isActive);
+    }
+}
