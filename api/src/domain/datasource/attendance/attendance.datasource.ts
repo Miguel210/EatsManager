07f2aee724481717ebf25a9d6360212b83615299
Miguel210 <mickyjunior04@gmail.com>
2024-10-22 11:19:33 -0600

    Created datasource and repository (attendance) files

---
+++ api/src/domain/datasource/attendance/attendance.datasource.ts
@@ -0,0 +1,15 @@
+import { CreateAttendanceDto } from "../../dtos/attendance/create-attendance.dto";
+import { UpdateAttendanceDto } from "../../dtos/attendance/update-attendance.dto";
+import { AttendanceEntity } from "../../entities/attendance.entity";
+
+
+
+export abstract class AttendanceDatasouce {
+
+    abstract create(dto: CreateAttendanceDto): Promise<AttendanceEntity>;
+
+    abstract getById(id: string): Promise<AttendanceEntity>;
+    abstract gets(form: any): Promise<AttendanceEntity[]>;
+    abstract update(dto: UpdateAttendanceDto): Promise<AttendanceEntity>;
+    abstract delete(id: string): Promise<AttendanceEntity>;
+}
