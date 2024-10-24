93fd0f54c560603f6cf7080c35694e17056ec775
Miguel210 <mickyjunior04@gmail.com>
2024-10-24 16:26:31 -0600

    Added new data in  documentEntity

--- api/src/domain/entities/movement.entity.ts
+++ api/src/domain/entities/movement.entity.ts
@@ -21,15 +21,15 @@ export class MovementEntity {
 
         const {id, personId, person, documentId, document, elaborateId, elaborate, amount, status, isActive, referenceId, reference, date} = object
 
-        if( !id ) throw CustomError.badRequest('Missing ');
-        if( !personId || !person ) throw CustomError.badRequest('Missing ');
-        if( !documentId || !document ) throw CustomError.badRequest('Missing ');
+        if( !id ) throw CustomError.badRequest('Missing id');
+        if( !personId || !person ) throw CustomError.badRequest('Missing personId');
+        if( !documentId || !document ) throw CustomError.badRequest('Missing documentId');
         //if( !elaborateId || !elaborate ) throw CustomError.badRequest('Missing ');
-        if( !amount ) throw CustomError.badRequest('Missing ');
-        if( !status ) throw CustomError.badRequest('Missing ');
-        if( isActive === undefined ) throw CustomError.badRequest('Missing ');
-        if( !referenceId || reference) throw CustomError.badRequest('Missing ');
-        if( date === undefined ) throw CustomError.badRequest('Missing ');
+        if( !amount ) throw CustomError.badRequest('Missing amount');
+        if( !status ) throw CustomError.badRequest('Missing status');
+        if( isActive === undefined ) throw CustomError.badRequest('Missing isActive');
+        //if( !referenceId || reference) throw CustomError.badRequest('Missing referenceId');
+        if( date === undefined ) throw CustomError.badRequest('Missing date');
 
         return new MovementEntity(id, personId || person, documentId || document, elaborateId || elaborate, amount, status, isActive, referenceId || reference, date);
     }
