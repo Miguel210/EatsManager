ec2a71b10c2105ab0f34b2a64ec02bca00823a55
Miguel210 <mickyjunior04@gmail.com>
2024-10-25 13:08:05 -0600

    Created movement  repository and datasource files

---
+++ api/src/domain/repositories/movement/movement.repository.ts
@@ -0,0 +1,15 @@
+import { CreateMovementDto } from "../../dtos/movement/create-movement.dto";
+import { UpdateMovementDto } from "../../dtos/movement/update-movement.dto";
+import { MovementEntity } from "../../entities/movement.entity";
+
+
+
+
+export abstract class MovementRepository {
+    
+    abstract create(dto: CreateMovementDto): Promise <MovementEntity>;
+    abstract getById(id: string): Promise <MovementEntity>;
+    abstract getAll(form: any): Promise <MovementEntity>;
+    abstract update(dto: UpdateMovementDto): Promise <MovementEntity>;
+
+}
