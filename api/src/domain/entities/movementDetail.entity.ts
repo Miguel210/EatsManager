449a902692b4d3c2c232e1b84172e0df5a56995f
Miguel Angel Lozano Alvarez <mickyjunior04@gmail.com>
2024-10-25 00:02:53 -0600

    Created movementDetail file

---
+++ api/src/domain/entities/movementDetail.entity.ts
@@ -0,0 +1,40 @@
+import { CustomError } from "../errors/custom.error";
+
+
+
+export class MovementDetailEntity {
+
+    constructor(
+        public id: string,
+        public movementId: string,
+        public quantity: number,
+        public productId: string,
+        public priceUnit: number,
+        public subTotal: number,
+        public tax: number,
+        public total: number,
+        public costUnit: number,
+        public promotionId: string,
+        public isActive: boolean
+    ) {}
+
+
+    static fromObject(object: {[key: string]: any}) {
+
+        const {id, movementId, movement, quantity, productId, product, priceUnit, subTotal, tax, total, costUnit, promotionId, promotion, isActive} = object;
+
+        if( !id ) throw CustomError.badRequest('Missing id');
+        if( !movementId && !movement ) throw CustomError.badRequest('Missing movementId');
+        if( !quantity ) throw CustomError.badRequest('Missing quantity');
+        if( productId && product ) throw CustomError.badRequest('Missing productId');
+        if( !priceUnit ) throw CustomError.badRequest('Missing priceUnit');
+        if( !subTotal ) throw CustomError.badRequest('Missing subTotal');
+        if( !tax ) throw CustomError.badRequest('Missing tax');
+        if( !total ) throw CustomError.badRequest('Missing total');
+        if( !costUnit ) throw CustomError.badRequest('Missing costUnit');
+       // if( !promotionId && promotion ) throw CustomError.badRequest('Missing promotionId');
+        if( isActive === undefined ) throw CustomError.badRequest('Missing isActive');
+
+        return new MovementDetailEntity(id, movementId || movement, quantity, productId || product, priceUnit, subTotal, tax, total, costUnit, promotionId || promotion, isActive)
+    }
+}
