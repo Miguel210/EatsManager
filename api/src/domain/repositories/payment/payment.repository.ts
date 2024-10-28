353facb5705eff163f4d07970b5f793c35ba0c10
Miguel210 <mickyjunior04@gmail.com>
2024-10-28 11:34:13 -0600

    Created payment datasource and repository files

---
+++ api/src/domain/repositories/payment/payment.repository.ts
@@ -0,0 +1,15 @@
+import { UpdateOrderpaymentDto } from "../../dtos/orderPayment/update-orderPayment.dto";
+import { CreatePaymentDto } from "../../dtos/payment/create-payment.dto";
+import { PaymentEntity } from "../../entities/payment.entity";
+
+
+
+export abstract class PaymentRepository {
+
+    abstract create(dto: CreatePaymentDto): Promise <PaymentEntity>;
+    abstract get(id: string): Promise <PaymentEntity>;
+    abstract getAll(form: any): Promise <PaymentEntity>;
+    abstract update(dto: UpdateOrderpaymentDto): Promise <PaymentEntity>;
+    abstract delete(id: string): Promise <PaymentEntity>;
+
+}
