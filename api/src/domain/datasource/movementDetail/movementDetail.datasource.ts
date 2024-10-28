0ae5c7f0afcc65082e49d65971d9f81cf1b0a833
Miguel210 <mickyjunior04@gmail.com>
2024-10-28 09:39:17 -0600

    Created movementDetail datasource and repository files

---
+++ api/src/domain/datasource/movementDetail/movementDetail.datasource.ts
@@ -0,0 +1,15 @@
+import { CreateMovementDetailDto } from "../../dtos/movementDetail/create-movementDetail";
+import { UpdateMovementDetailDto } from "../../dtos/movementDetail/update-movementDetail";
+import { MovementDetailEntity } from "../../entities/movementDetail.entity";
+
+
+
+
+export abstract class MovemenetDetailDatasource {
+
+    abstract create(dto: CreateMovementDetailDto): Promise<MovementDetailEntity>;
+    abstract get(id: string): Promise<MovementDetailEntity>;
+    abstract getAll(form: any): Promise<MovementDetailEntity[]>;
+    abstract update(dto: UpdateMovementDetailDto): Promise<MovementDetailEntity>;
+    abstract delete(id: string): Promise<MovementDetailEntity>;
+}
