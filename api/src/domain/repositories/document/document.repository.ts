4ec7e3263c3ed10e21162f04249f9dafd07bde5f
Miguel Angel Lozano Alvarez <mickyjunior04@gmail.com>
2024-10-27 23:10:51 -0600

    Created document datasource and repository files

---
+++ api/src/domain/repositories/document/document.repository.ts
@@ -0,0 +1,16 @@
+import { CreateDocumentDto } from "../../dtos/document/create-document.dto";
+import { UpdateDocumentDto } from "../../dtos/document/update-document.dto";
+import { DocumentEntity } from '../../entities/document.entity';
+
+
+
+export abstract class DocumentRepository {
+
+    abstract create(dto: CreateDocumentDto): Promise<DocumentEntity>;
+
+    abstract get(id: string): Promise<DocumentEntity>;
+    abstract getAll(form: any): Promise<DocumentEntity>;
+    abstract update(dto: UpdateDocumentDto): Promise<DocumentEntity>;
+    abstract delete(id: string): Promise<DocumentEntity>;
+
+}
