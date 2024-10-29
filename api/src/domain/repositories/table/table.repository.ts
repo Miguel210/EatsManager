0b23705d734f5e7b93f58a79ff723f75227851af
Miguel210 <mickyjunior04@gmail.com>
2024-10-29 14:26:54 -0600

    Created table repository and datasource files

---
+++ api/src/domain/repositories/table/table.repository.ts
@@ -0,0 +1,15 @@
+import { CreateTableDto } from "../../dtos/table/create-table.dto";
+import { UpdateTableDto } from "../../dtos/table/update-table.dto";
+import { TableEntity } from "../../entities/table.entity";
+
+
+export abstract class TableRepository {
+
+    abstract create(dto: CreateTableDto): Promise <TableEntity>;
+
+    abstract get(id: string): Promise <TableEntity>;
+    abstract getAll(form: any): Promise <TableEntity>;
+    abstract update(dto: UpdateTableDto): Promise <TableEntity>;
+    abstract delete(id: string): Promise <TableEntity>;
+
+}
