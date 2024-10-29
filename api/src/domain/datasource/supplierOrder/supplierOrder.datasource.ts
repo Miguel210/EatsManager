765bafe1fd43f9c817b2990a84fb2797d9825c16
Miguel210 <mickyjunior04@gmail.com>
2024-10-29 14:00:14 -0600

    Created supplierOrder repository and datasource files

---
+++ api/src/domain/datasource/supplierOrder/supplierOrder.datasource.ts
@@ -0,0 +1,15 @@
+import { CreateSupplierOrderDto } from "../../dtos/supplierOrder/create-supplierOrder.dto";
+import { UpdateSupplierOrderDto } from "../../dtos/supplierOrder/update-supplierOrder.dto";
+import { SupplierOrderEntity } from "../../entities/supplierOrder.entity";
+
+
+export abstract class SupplierOrderDatasource {
+
+    abstract create(dto: CreateSupplierOrderDto): Promise<SupplierOrderEntity>;
+
+    abstract get(id: string): Promise<SupplierOrderEntity>;
+    abstract getAll(form: any): Promise<SupplierOrderEntity[]>;
+    abstract update(dto: UpdateSupplierOrderDto): Promise<SupplierOrderEntity>;
+    abstract delete(id: string): Promise<SupplierOrderEntity>;
+
+}
