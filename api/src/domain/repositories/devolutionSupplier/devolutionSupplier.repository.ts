4e3d4c2f4468cfddfb9fbe646d4eb4dbed694fd6
Miguel210 <mickyjunior04@gmail.com>
2024-10-29 14:13:00 -0600

    Created devolutionSupplier repository and datasource files

---
+++ api/src/domain/repositories/devolutionSupplier/devolutionSupplier.repository.ts
@@ -0,0 +1,15 @@
+import { CreateDevolutionSupplierDto } from "../../dtos/devolutionSupplier/create-devolutionSupplier.dto";
+import { UpdateDevolutionSupplierDto } from "../../dtos/devolutionSupplier/update-devolutionSupplier.dto";
+import { DevolutionSupplierEntity } from "../../entities/devolutionSupplier.entity";
+
+
+export abstract class DevolutionSupplierRepository {
+
+    abstract create(dto: CreateDevolutionSupplierDto): Promise<DevolutionSupplierEntity>;
+
+    abstract get(id: string): Promise<DevolutionSupplierEntity>;
+    abstract getAll(form: any): Promise<DevolutionSupplierEntity[]>;
+    abstract update(dto: UpdateDevolutionSupplierDto): Promise<DevolutionSupplierEntity>;
+    abstract delete(id: string): Promise<DevolutionSupplierEntity>;
+
+}
