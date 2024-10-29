ed42a9c6391101065f34c160f583b0b716400e93
Miguel210 <mickyjunior04@gmail.com>
2024-10-29 12:56:24 -0600

    Created devolutionClietEntity and devolutionSupplierEntity files

---
+++ api/src/domain/entities/devolutionClient.entity.ts
@@ -0,0 +1,31 @@
+import { CustomError } from "../errors/custom.error";
+
+
+
+
+
+export class DevolutionClientEntity{
+
+    constructor(
+        public id: string,
+        public movementId: string,
+        public invoiceFolio: string,
+        public paymentDate: Date,
+        public status: string,
+        public isActive: boolean
+    ) {}
+
+    static create(object: {[key: string]: any}) {
+
+        const {id, movementId, movement, invoiceFolio, paymentDate, status, isActive} = object;
+    
+        if( !id ) CustomError.badRequest('Missing id');
+        if( !movementId && !movement ) CustomError.badRequest('Missing movementId');
+        if( !invoiceFolio ) CustomError.badRequest('Missing invoiceFolio');
+        if( !paymentDate ) CustomError.badRequest('Missing paymentDate');
+        if( !status ) CustomError.badRequest('Missing status');
+        if( isActive === undefined ) CustomError.badRequest('Missing isActive');
+
+        return new DevolutionClientEntity(id, movementId || movement, invoiceFolio, paymentDate, status, isActive);
+    }
+}
