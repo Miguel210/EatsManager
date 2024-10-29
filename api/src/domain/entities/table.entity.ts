253f152034baf811e42177142341aba753291d71
Miguel210 <mickyjunior04@gmail.com>
2024-10-29 13:03:40 -0600

    Created tableEntity file

---
+++ api/src/domain/entities/table.entity.ts
@@ -0,0 +1,27 @@
+import { CustomError } from "../errors/custom.error";
+
+
+
+export class TableEntity {
+
+    constructor(
+        public id: string,
+        public description: string,
+        public status: string,
+        public mainTable: string,
+        public isActive: boolean
+    ) {}
+
+    static fromObject(object: {[key:string]: any}) {
+
+        const {id, description, status, mainTable, isActive} = object;
+
+        if( !id ) throw CustomError.badRequest('Missing id');
+        if( !description ) throw CustomError.badRequest('Missing description');
+        if( !status ) throw CustomError.badRequest('Missing status');
+        //if( !id ) throw CustomError.badRequest('Missing id');
+        if( isActive === undefined ) throw CustomError.badRequest('Missing isActive');
+
+        return new TableEntity(id, description, status, mainTable, isActive);
+    }
+}
