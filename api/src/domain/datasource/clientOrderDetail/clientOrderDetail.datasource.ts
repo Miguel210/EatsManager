654ea59d77f11b6734160515fc4457e003bb67bf
Miguel210 <mickyjunior04@gmail.com>
2024-10-29 14:24:17 -0600

    Created devolutionClientDetail repository and datasource files

---
+++ api/src/domain/datasource/clientOrderDetail/clientOrderDetail.datasource.ts
@@ -0,0 +1,16 @@
+import { CreateClientOrderDetailDto } from "../../dtos/clientOrderDetail/create-clientOrderDetail.dto";
+import { UpdateClientOrderDetailDto } from "../../dtos/clientOrderDetail/update-clientOrderDetail.dto";
+import { ClientOrderDetailEntity } from "../../entities/clientOrderDetail.entity";
+
+
+
+export abstract class ClientOrderDetailDatasource {
+
+    abstract create(dto: CreateClientOrderDetailDto): Promise <ClientOrderDetailEntity>;
+
+    abstract get(id: string): Promise <ClientOrderDetailEntity>;
+    abstract getAll(form: any): Promise <ClientOrderDetailEntity>;
+    abstract update(dto: UpdateClientOrderDetailDto): Promise <ClientOrderDetailEntity>;
+    abstract delete(id: string): Promise <ClientOrderDetailEntity>;
+
+}
