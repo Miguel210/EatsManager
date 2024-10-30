8104a98422dd3b26cbe36bd9f7e9c444ff8f19be
Miguel Angel Lozano Alvarez <mickyjunior04@gmail.com>
2024-10-30 01:03:13 -0600

    Created clientOrder service file

---
+++ api/src/presentation/services/clientOrder.service.ts
@@ -0,0 +1,31 @@
+import { ClientOrderRepositoryImpl } from "../../infraestructure/clientOrder/repository/clientOrder.repository.impl";
+
+
+
+export class ClientOrderService {
+
+    constructor(
+        private readonly repository: ClientOrderRepositoryImpl
+    ) {}
+
+    public async create() {
+
+    }
+
+    public async get() {
+        
+    }
+
+    public async getAll() {
+        
+    }
+
+    public async update() {
+        
+    }
+
+    public async delete() {
+        
+    }
+
+}
