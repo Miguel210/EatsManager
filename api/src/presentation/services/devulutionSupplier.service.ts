83b94907534602497ec5c40b1eec61373dd7ff8b
Miguel Angel Lozano Alvarez <mickyjunior04@gmail.com>
2024-10-30 00:58:49 -0600

    Created devolutionSupplier service file

---
+++ api/src/presentation/services/devulutionSupplier.service.ts
@@ -0,0 +1,31 @@
+import { DevolutionSupplierRepositoryImpl } from '../../infraestructure/devolutionSupplier/repository/devolutionSupplier.repository.impl';
+
+
+
+export class DevolutionSupplierService {
+
+    constructor(
+        private readonly repository: DevolutionSupplierRepositoryImpl
+    ) {}
+
+    public async create() {
+
+    }
+
+    public async get() {
+        
+    }
+
+    public async getAll() {
+        
+    }
+
+    public async update() {
+        
+    }
+
+    public async delete() {
+        
+    }
+
+}
