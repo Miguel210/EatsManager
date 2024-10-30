ad36d51b5699ccc734e356a119350cb56b91cdab
Miguel Angel Lozano Alvarez <mickyjunior04@gmail.com>
2024-10-30 01:06:21 -0600

    Created table service file

---
+++ api/src/presentation/services/table.service.ts
@@ -0,0 +1,31 @@
+import { tableRepositoryImpl } from "../../infraestructure/table/repository/tableRepository.impl";
+
+
+
+export class tableService {
+
+    constructor(
+        private readonly repository: tableRepositoryImpl
+    ) {}
+
+    public async create() {
+
+    }
+
+    public async get() {
+        
+    }
+
+    public async getAll() {
+        
+    }
+
+    public async update() {
+        
+    }
+
+    public async delete() {
+        
+    }
+
+}
