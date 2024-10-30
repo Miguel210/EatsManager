0ada4c78db1c68e7c51b86659fbed76f8e2e4676
Miguel Angel Lozano Alvarez <mickyjunior04@gmail.com>
2024-10-30 01:00:39 -0600

    Created devolutionClientr service file

---
+++ api/src/presentation/services/devolutionClient.service.ts
@@ -0,0 +1,31 @@
+import { DevolutionClientRepositoryImpl } from '../../infraestructure/devolutionClient/repository/devolutionClient.repository.impl';
+
+
+
+export class DevolutionClientService {
+
+    constructor(
+        private readonly repository: DevolutionClientRepositoryImpl
+    ) {}
+
+    public async create() {
+
+    }
+
+    public async get() {
+        
+    }
+
+    public async getAll() {
+        
+    }
+
+    public async update() {
+        
+    }
+
+    public async delete() {
+        
+    }
+
+}
