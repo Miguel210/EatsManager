89c66385d6777b368e0077311bb65292893ef60d
Miguel210 <mickyjunior04@gmail.com>
2024-10-30 09:42:44 -0600

    Added folio field in document table and updated entity, dto, impl document files

--- api/src/domain/dtos/document/update-document.dto.ts
+++ api/src/domain/dtos/document/update-document.dto.ts
@@ -7,17 +7,19 @@ export class UpdateDocumentDto {
     constructor(
         public id: string,
         public description: string,
-        public isActive: boolean
+        public isActive: boolean,
+        public folio: number
     ) {}
 
     static create(props: {[key: string]: any}): [string?, UpdateDocumentDto?] {
 
-        const {id, description, isActive} = props;
+        const {id, description, isActive, folio} = props;
 
         if( !id ) throw ['id is requerid'];
         if( !description ) throw ['Description is requerid'];
         if( isActive === undefined ) throw ['isActive is requerid'];
+        if( !folio ) throw ['folio is requerid'];
 
-        return [undefined, new UpdateDocumentDto(id, description,isActive)]
+        return [undefined, new UpdateDocumentDto(id, description,isActive, folio)]
     }
 }
