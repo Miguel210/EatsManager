c8b1c8e6c8e5a9b8ebf1cc3459bb0b6048a7c21e
Miguel Angel Lozano Alvarez <mickyjunior04@gmail.com>
2024-11-03 23:05:50 -0600

    Deleted vue

--- eatsmanager/src/components/loginComponents.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div>
-        <label for="username">username</label>
-        <input type="text">
-        <label for="password">password</label>
-        <input type="password">
-    </div>
-</template>
