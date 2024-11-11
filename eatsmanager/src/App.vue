4199caa1f1df9aeb947e0b3e32fdf5b607ebe4f5
Miguel210 <mickyjunior04@gmail.com>
2024-11-11 14:09:28 -0600

    created vue (eatsmanager) file

---
+++ eatsmanager/src/App.vue
@@ -0,0 +1,13 @@
+
+<template>
+  <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <RouterView />
+</template>
+
+  <script setup lang="ts">
+  import { RouterView } from 'vue-router'
+  </script>
+
+<style scoped></style>
