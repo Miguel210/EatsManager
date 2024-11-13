9e672770bfa55513391171a246c1ccd8e00aa45f
Miguel210 <mickyjunior04@gmail.com>
2024-11-13 14:40:13 -0600

    Changed text-color in MenuItem.vue

--- eatsmanager/src/modules/home/components/SideMenu.vue
+++ eatsmanager/src/modules/home/components/SideMenu.vue
@@ -1,10 +1,10 @@
 <template>
-<aside class="w-72 bg-[#1c212c] min-h-full h-screen flex flex-col items-center pt-5 pb-2 space-y-7">
+<aside class="w-72 bg-red-400 min-h-full h-screen flex flex-col items-center pt-5 pb-2 space-y-7">
 
 <!-- menu items -->
-  <div class="w-full pr-3 flex flex-col gap-y-1 text-gray-500 fill-gray-500 text-sm">
+  <div class="w-full pr-3 flex flex-col gap-y-1 text-white fill-white text-sm">
 
-    <div class="font-QuicksandMedium pl-4 text-gray-400/60 text-xs text-[11px] uppercase">Menu</div>
+    <div class="font-QuicksandMedium pl-4 text-black text-xs text-[11px] uppercase">Menu</div>
 
     <MenuItem />
     <MenuItem />
