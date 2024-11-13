10caaa1ad633d1928905947e43e3494941bd387a
Miguel210 <mickyjunior04@gmail.com>
2024-11-13 16:50:39 -0600

    Added DataTables component

---
+++ eatsmanager/src/modules/common/components/DataTable.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import DataTable from 'datatables.net-dt';
+let table = new DataTable('#myTable');
+
+</script>
+
+<style scoped>
+
+</style>
+
+
