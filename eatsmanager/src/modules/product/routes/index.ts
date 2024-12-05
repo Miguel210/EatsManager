f6fc0b6ac77759b9f13f00740f44ee4acb24c9b9
Miguel210 <mickyjunior04@gmail.com>
2024-12-05 00:52:46 -0600

    Created routesProduct and routes children

--- eatsmanager/src/modules/product/routes/index.ts
+++ eatsmanager/src/modules/product/routes/index.ts
@@ -12,12 +12,41 @@ export const categoryRoutes: RouteRecordRaw = {
       name: 'productoMenu',
       component: () => import('@/modules/product/views/productMenuView.vue'),
     },
+//todo product
+    // {
+    //   path: 'productos/:productId',
+    //   name: 'productForm',
+    //   props: true,
+    //   component: () => import('@/modules/product/views/product/formProdView.vue')
+    // },
+
 //todo category
     // {
     //   path: 'categorias/:categoryId',
     //   name: 'categoryForm',
     //   props: true,
     //   component: () => import('@/modules/product/views/category/formCatView.vue')
+    // },
+//todo product type
+    // {
+    //   path: 'tipos/:producttypeId',
+    //   name: 'productTypeForm',
+    //   props: true,
+    //   component: () => import('@/modules/product/views/type/formTypeView.vue')
+    // },
+//todo inventory
+    // {
+    //   path: 'inventarios/:inventoryId',
+    //   name: 'inventoryForm',
+    //   props: true,
+    //   component: () => import('@/modules/product/views/inventory/formInvView.vue')
+    // },
+//todo garrision
+    // {
+    //   path: 'guarniciones/:garrisiomId',
+    //   name: 'garrisionForm',
+    //   props: true,
+    //   component: () => import('@/modules/product/views/garrision/formGarrView.vue')
     // },
 
   ]
